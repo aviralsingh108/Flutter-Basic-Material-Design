1fe722e8b473e6da59f1741ead78142b45e2f46e
aviralsingh108 <admiralaviral@gmail.com>
2020-07-08 16:43:30 +0530

    Applying Material Design to the Application

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,25 @@
 import "package:flutter/material.dart";
-void main()
-{
-  runApp(
-    Center(
-        child:Text(
-          "Hello Flutter",
-          textDirection: TextDirection.ltr
+void main() => runApp(new MyFlutterApp());
+
+class MyFlutterApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "My Flutter App",
+      home: Scaffold(
+          appBar: AppBar(title: Text("My First App Screen"),),
+          body: Material(
+            color: Colors.lightBlueAccent,
+            child: Center(
+              child:Text(
+                "Hello Flutter",
+                textDirection: TextDirection.ltr,
+                style: TextStyle(color: Colors.white, fontSize: 40.0),
+              ),
+            ) ,
+          )
       ),
-    )
-  );
+    );
+  }
 }
